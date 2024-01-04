e0dc9921e52dae9daef92e36cbb58e8ba04636ba
calvarez123 <calvarezmartinez.cf@iesesteveterradas.cat>
2024-01-04 18:48:15 +0100

    adasa

--- editor_base/lib/layout_sidebar_shapes.dart
+++ editor_base/lib/layout_sidebar_shapes.dart
@@ -1,9 +1,9 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:editor_base/SidebarShapePainter.dart';
+import 'package:flutter/material.dart';
 import 'package:editor_base/app_data.dart';
 import 'package:editor_base/util_shape.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_cupertino_desktop_kit/cdk_theme.dart';
-import 'package:flutter_cupertino_desktop_kit/cdk_theme_notifier.dart';
 import 'package:provider/provider.dart';
 
 class LayoutSidebarShapes extends StatelessWidget {
@@ -11,67 +11,93 @@ class LayoutSidebarShapes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    AppData appData = Provider.of<AppData>(context);
+    return Consumer<AppData>(
+      builder: (context, appData, _) {
+        Color customAccentColor = Color.fromARGB(255, 183, 182, 182);
+        double itemHeight =
+            100.0; // Ajusta la altura de cada elemento de la lista según tus necesidades
 
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('List of shapes',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            SizedBox(
-              height: 500,
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: appData.shapesList.length,
-                itemBuilder: (context, index) {
-                  Shape shape = appData.shapesList[index];
-                  return GestureDetector(
-                    onTap: () {
-                      if (index == appData.selectedShapeIndex) {
-                        appData.selectedShapeIndex = -1;
-                      } else {
-                        appData.selectShape(index);
-                      }
-                      appData.notifyListeners();
-                    },
-                    child: Container(
-                      margin: EdgeInsets.all(8.0),
-                      padding: EdgeInsets.all(12.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.lightBlueAccent),
-                        color: index == appData.selectedShapeIndex
-                            ? Colors.lightBlueAccent
-                            : Colors.blueGrey[800],
-                        borderRadius: BorderRadius.circular(16.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            blurRadius: 4.0,
-                            offset: Offset(2, 2),
+        return SizedBox(
+          width: double.infinity,
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'List of shapes',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: appData.shapesList.length * itemHeight,
+                  child: ListView.separated(
+                    itemCount: appData.shapesList.length,
+                    separatorBuilder: (context, index) => Divider(),
+                    itemBuilder: (context, index) {
+                      Shape shape = appData.shapesList[index];
+                      bool isSelected = index == appData.selectedShapeIndex;
+
+                      return GestureDetector(
+                        onTap: () {
+                          if (isSelected) {
+                            appData.selectedShapeIndex = -1;
+                          } else {
+                            appData.selectShape(index);
+                          }
+                          appData.notifyListeners();
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(12.0),
+                          decoration: BoxDecoration(
+                            color: isSelected
+                                ? customAccentColor
+                                : Colors.transparent,
+                            border: Border.all(
+                              color: customAccentColor,
+                              width: 2.0,
+                            ),
                           ),
-                        ],
-                      ),
-                      child: Center(
-                        child: Text(
-                          '${shape.position}',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                          child: Row(
+                            children: [
+                              // Miniatura del shape
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  color: Colors
+                                      .white, // Color de fondo de la miniatura
+                                ),
+                                child: CustomPaint(
+                                  painter: SidebarShapePainter(shape),
+                                ),
+                              ),
+                              // Información del shape
+                              Expanded(
+                                child: Center(
+                                  child: Text(
+                                    'Shape ${index + 1}', // Puedes personalizar el contenido aquí
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: isSelected
+                                          ? Colors.white
+                                          : Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
