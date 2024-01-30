d458ddf62f1fb2ff899c369127b5b3bf9cd4e7a5
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2024-01-30 18:39:27 +0100

    ads

--- editor_base/lib/main.dart
+++ editor_base/lib/main.dart
@@ -27,56 +27,57 @@ void main() async {
 
   runApp(Focus(
     onKey: (FocusNode node, RawKeyEvent event) {
-  bool isControlPressed = (Platform.isMacOS && event.isMetaPressed) ||
-      (Platform.isLinux && event.isControlPressed) ||
-      (Platform.isWindows && event.isControlPressed);
-  bool isShiftPressed = event.isShiftPressed;
-  bool isZPressed = event.logicalKey == LogicalKeyboardKey.keyZ;
-  bool isDeletePressed = event.logicalKey == LogicalKeyboardKey.delete;
-  bool isBackspacePressed = event.logicalKey == LogicalKeyboardKey.backspace;
-  bool isCPressed = event.logicalKey == LogicalKeyboardKey.keyC;
-  bool isVPressed = event.logicalKey == LogicalKeyboardKey.keyV;
+      bool isControlPressed = (Platform.isMacOS && event.isMetaPressed) ||
+          (Platform.isLinux && event.isControlPressed) ||
+          (Platform.isWindows && event.isControlPressed);
+      bool isShiftPressed = event.isShiftPressed;
+      bool isZPressed = event.logicalKey == LogicalKeyboardKey.keyZ;
+      bool isDeletePressed = event.logicalKey == LogicalKeyboardKey.delete;
+      bool isBackspacePressed =
+          event.logicalKey == LogicalKeyboardKey.backspace;
+      bool isCPressed = event.logicalKey == LogicalKeyboardKey.keyC;
+      bool isVPressed = event.logicalKey == LogicalKeyboardKey.keyV;
 
-  if (event is RawKeyDownEvent) {
-    if (isControlPressed && isDeletePressed) {
-      if (appData.selectedShapeIndex != -1) {
-        var action =
-            ActionRemoveShape(appData, appData.shapesList[appData.selectedShapeIndex]);
-        appData.actionManager.register(action);
-        return KeyEventResult.handled;
-      }
-    } else if (isControlPressed && isBackspacePressed) {
-      if (appData.selectedShapeIndex != -1) {
-        var action =
-            ActionRemoveShape(appData, appData.shapesList[appData.selectedShapeIndex]);
-        appData.actionManager.register(action);
-        return KeyEventResult.handled;
-      }
-    } else if (isControlPressed && isZPressed && !isShiftPressed) {
-      appData.actionManager.undo();
-      return KeyEventResult.handled;
-    } else if (isControlPressed && isShiftPressed && isZPressed) {
-      appData.actionManager.redo();
-      return KeyEventResult.handled;
-    } else if (isControlPressed && isCPressed) {
-      _copyToClipboard(appData);
-      // Handle Control+C (copy) logic here
-      return KeyEventResult.handled;
-    } else if (isControlPressed && isVPressed) {
-      // Handle Control+V (paste) logic here
-      _pasteFromClipboard(appData);
-      return KeyEventResult.handled;
-    }
+      if (event is RawKeyDownEvent) {
+        if (isControlPressed && isDeletePressed) {
+          if (appData.selectedShapeIndex != -1) {
+            var action = ActionRemoveShape(
+                appData, appData.shapesList[appData.selectedShapeIndex]);
+            appData.actionManager.register(action);
+            return KeyEventResult.handled;
+          }
+        } else if (isControlPressed && isBackspacePressed) {
+          if (appData.selectedShapeIndex != -1) {
+            var action = ActionRemoveShape(
+                appData, appData.shapesList[appData.selectedShapeIndex]);
+            appData.actionManager.register(action);
+            return KeyEventResult.handled;
+          }
+        } else if (isControlPressed && isZPressed && !isShiftPressed) {
+          appData.actionManager.undo();
+          return KeyEventResult.handled;
+        } else if (isControlPressed && isShiftPressed && isZPressed) {
+          appData.actionManager.redo();
+          return KeyEventResult.handled;
+        } else if (isControlPressed && isCPressed) {
+          _copyToClipboard(appData);
+          // Handle Control+C (copy) logic here
+          return KeyEventResult.handled;
+        } else if (isControlPressed && isVPressed) {
+          // Handle Control+V (paste) logic here
+          _pasteFromClipboard(appData);
+          return KeyEventResult.handled;
+        }
 
-    if (event.logicalKey == LogicalKeyboardKey.altLeft) {
-      appData.isAltOptionKeyPressed = true;
-    }
-    } else if (event is RawKeyUpEvent) {
-      if (event.logicalKey == LogicalKeyboardKey.altLeft) {
-        appData.isAltOptionKeyPressed = false;
+        if (event.logicalKey == LogicalKeyboardKey.altLeft) {
+          appData.isAltOptionKeyPressed = true;
+        }
+      } else if (event is RawKeyUpEvent) {
+        if (event.logicalKey == LogicalKeyboardKey.altLeft) {
+          appData.isAltOptionKeyPressed = false;
+        }
       }
-    }
-    return KeyEventResult.ignored;
+      return KeyEventResult.ignored;
     },
     child: ChangeNotifierProvider(
       create: (context) => appData,
@@ -108,16 +109,17 @@ void _copyToClipboard(AppData appData) {
 
 Future<void> _pasteFromClipboard(AppData appData) async {
   try {
-    String? jsonString = await Clipboard.getData(Clipboard.kTextPlain)?.then((value) => value?.text);
+    String? jsonString = await Clipboard.getData(Clipboard.kTextPlain)
+        ?.then((value) => value?.text);
     if (jsonString != null) {
       Map<String, dynamic> map = jsonDecode(jsonString);
       if (map['type'] == 'shape_drawing') {
         var shape = Shape.fromMap(map);
+
         appData.actionManager.register(ActionAddNewShape(appData, shape));
       }
     }
   } catch (e) {
-    
     print("Error pasting from clipboard: $e");
   }
 }
